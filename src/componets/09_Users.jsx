12f037dd969d4063e92cfbad4d5696132c81e791
X <pikalov.ea97@gmail.com>
2026-04-13 22:45:38 +0300

    test

--- src/componets/09_Users.jsx
+++ src/componets/09_Users.jsx
@@ -15,21 +15,21 @@ export const Users = () => {
             <ul className={s.list}>
                 <li className={s.user} style={{ backgroundColor: '#fef2f2', borderColor: ' #fca5a5', border: '1px solid #ef4444' }}>
                     <span className={s.name}>Иван Иванов</span>
-                    <span className={s.job_title}>Резчик х/м</span>
+                    {/* <span className={s.job_title}>Резчик х/м</span> */}
                     <span className={s.deadline_siz} style={{ backgroundColor: '#ef4444', boxShadow: '0 0 8px rgba(239, 68, 68, 0.4)' }}>
                         0
                     </span>
                 </li>
                 <li className={s.user} style={{ backgroundColor: '#fffbeb', borderColor: ' #fca5a5', border: '1px solid #fcd34d' }}>
                     <span className={s.name}>Тест Тест</span>
-                    <span className={s.job_title}>Резчик х/м</span>
+                    {/* <span className={s.job_title}>Резчик х/м</span> */}
                     <span className={s.deadline_siz} style={{ backgroundColor: '#f59e0b', color: '#ffffff', boxShadow: '0 0 8px rgba(245, 158, 11, 0.4)' }}>
                         10
                     </span>
                 </li>
                 <li className={s.user}>
                     <span className={s.name}>Тест Т</span>
-                    <span className={s.job_title}>Вальцовщик</span>
+                    {/* <span className={s.job_title}>Вальцовщик</span> */}
                     <span className={s.deadline_siz}>365</span>
                 </li>
             </ul>
