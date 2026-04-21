4bf8f3b21ef3608339ad2f3baf29a20b07ce3cba
X <pikalov.ea97@gmail.com>
2026-04-21 18:41:01 +0300

    new1

--- client/src/componets/16_Modal_selection_siz.jsx
+++ client/src/componets/16_Modal_selection_siz.jsx
@@ -1,30 +1,29 @@
 import { X } from 'lucide-react';
-import Siz_store from '../store/02_Siz_store.js'
+import Siz_store from '../store/02_Siz_store.js';
 import s from '../styles/16_modal_selection_siz.module.css';
 
 export const Modal_selection_siz = ({ set_modal_active_step }) => {
-
     const test = (id) => {
-        set_modal_active_step('Выбор параметров')
-        Siz_store.set_current(id)
-    }
+        set_modal_active_step('Выбор параметров');
+        Siz_store.set_current(id);
+    };
 
     return (
-        <div className={s.selection_modal}>
-            <div className={s.modal_header}>
-                <h3 className={s.modal_title}>Выдача СИЗ</h3>
-                <X className={s.modal_close_btn} size={20} onClick={() => set_modal_active_step(null)} />
+        <div className={s.container}>
+            <div className={s.wrapper_header}>
+                <h3 className={s.header}>Выдача СИЗ</h3>
+                <X className={s.close_btn} size={20} onClick={() => set_modal_active_step(null)} />
             </div>
-            <div className={s.modal_content}>
+            <div className={s.content}>
                 {Siz_store.get_all_data().map((item) => {
                     return (
-                        <div className={s.modal_item} key={item.id} onClick={() => test(item.id)}>
-                            <img className={s.modal_img} src={item.img} />
-                            <h4 className={s.modal_name}>{item.title}</h4>
+                        <div key={item.id} className={s.wrapper_item} onClick={() => test(item.id)}>
+                            <img className={s.img} src={item.img} />
+                            <h4 className={s.titlke}>{item.title}</h4>
                         </div>
-                    )
+                    );
                 })}
             </div>
         </div>
-    )
-}
+    );
+};
